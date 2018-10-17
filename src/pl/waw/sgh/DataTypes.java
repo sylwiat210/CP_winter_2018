0a8e14d257939d52414e37bbf5efefd0bb0d17cb
sylwiat210 <Roza210210>
2018-10-17 11:40:44 +0200

    Adding sth

---
+++ src/pl/waw/sgh/DataTypes.java
@@ -0,0 +1,39 @@
+package pl.waw.sgh;
+
+public class DataTypes {
+
+    /*
+    This is comment
+     */
+// psvm
+  public static void main (String[] args) {
+      byte b1 = 15;
+      byte b2 = 120;
+      int i1 = 434535642;
+      int i2 = 94565454;
+
+      long i3=i1;
+      //i3=i3+i2;
+      i3 +=i2;
+
+      //sout
+
+      System.out.println("i3:" +i3);
+
+      int a =5;
+      int b,bb;
+      b = a++;k
+    //  a= 5;
+      bb = ++a;
+
+      System.out.println ("b=" +b);
+      System.out.println ("bb=" +bb);
+
+      double d1 = 2;
+      double d2 = 3;
+
+      double d3= Math.pow(d1,d2); //2^3
+
+      System.out.println("d3=" +d3);
+  }
+}
